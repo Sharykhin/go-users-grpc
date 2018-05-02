8e85bdd81bb40bda990de7c1294c269be354f8d4
mac-222 <mac-222@mac-222s-Mac-mini.local>
2018-05-02 11:31:35 +0300

    added some comments

--- server/entity/user.go
+++ server/entity/user.go
@@ -2,7 +2,6 @@ package entity
 
 import (
 	"context"
-
 	"time"
 
 	pb "github.com/Sharykhin/go-users-grpc/proto"
@@ -10,10 +9,12 @@ import (
 )
 
 const (
+	// TimeFormat is a common time format that must be used for all times that should be represent as a string
 	TimeFormat = "2006-01-02T15:04:05"
 )
 
 type (
+	// User is a struct that represents basic entity for the current grpc server
 	User struct {
 		ID        bson.ObjectId `bson:"_id"`
 		Name      string        `bson:"name"`
@@ -22,12 +23,21 @@ type (
 		CreatedAt time.Time     `bson:"created_at"`
 		DeletedAt NullTime      `bson:"deleted_at"`
 	}
-
+	// NullTime implements setter and getter for bson to provide nullable value
 	NullTime struct {
 		Time time.Time
 	}
+
+	// UserService is a general interface of User entity
+	UserService interface {
+		List(ctx context.Context, limit, offset int64) ([]User, error)
+		Create(ctx context.Context, in *pb.CreateUserRequest) (*User, error)
+		Update(ctx context.Context, ID string, in *pb.UpdateUserRequest) error
+		Remove(ctx context.Context, ID string) error
+	}
 )
 
+// GetBSON converts value that should be saved right in a mongodb
 func (t NullTime) GetBSON() (interface{}, error) {
 	if t.Time.IsZero() {
 		return nil, nil
@@ -35,6 +45,7 @@ func (t NullTime) GetBSON() (interface{}, error) {
 	return t.Time.UTC(), nil
 }
 
+// SetBSON converts value from mongodb to a struct
 func (t *NullTime) SetBSON(raw bson.Raw) error {
 	var tt time.Time
 	err := raw.Unmarshal(&tt)
@@ -45,16 +56,10 @@ func (t *NullTime) SetBSON(raw bson.Raw) error {
 	return nil
 }
 
+// String custom stringer implementation to return empty string instead of zero time
 func (t NullTime) String() string {
 	if t.Time.IsZero() {
 		return ""
 	}
 	return t.Time.UTC().Format(TimeFormat)
-}
-
-type UserService interface {
-	List(ctx context.Context, limit, offset int64) ([]User, error)
-	Create(ctx context.Context, in *pb.CreateUserRequest) (*User, error)
-	Update(ctx context.Context, ID string, in *pb.UpdateUserRequest) error
-	Remove(ctx context.Context, ID string) error
 }
